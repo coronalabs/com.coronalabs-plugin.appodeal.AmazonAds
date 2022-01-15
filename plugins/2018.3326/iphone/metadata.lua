c7b02e62380a04744c97af821135413f94708445
Scott Harrison <scottrules44@gmail.com>
2022-01-15 11:29:46 -0500

    not currently by supported by Appodeal replacing with stub

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -3,8 +3,8 @@ local metadata =
 	plugin =
 	{
 		format = 'staticLibrary',
-		staticLibs = { 'APDAmazonAdsAdapter', },
-		frameworks = { 'AmazonAd', 'DTBiOSSDK', },
+		staticLibs = { },
+		frameworks = { },
 		frameworksOptional = {},
 	},
 }
